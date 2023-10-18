09b070c392c8eb028e0ed6c0e496a6e528019300
Nerscy <adriannieto1@hotmail.es>
2023-10-18 16:07:26 +0200

    ticket 1

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas
-import numpy
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-st.set_option('deprecation.showPyplotGlobalUse', False)
 '''
-Cargamos los datos
+Visualizamos la variable Clase
 '''
-train_df = pd.read_csv('data/train.csv')
-test_df = pd.read_csv('data/test.csv')
+survived_class = train_df[train_df['Survived']==1]['Pclass'].value_counts(sort=False)
+dead_class = train_df[train_df['Survived']==0]['Pclass'].value_counts(sort=False)
+class_df = pd.DataFrame([survived_class,dead_class],index=['Survived','Dead'])
+class_df.plot(kind='bar', stacked=True)
+plt.xlabel('Class')
+plt.ylabel('Number of passengers')
+plt.title('Survival rate by class')
+st.pyplot()
